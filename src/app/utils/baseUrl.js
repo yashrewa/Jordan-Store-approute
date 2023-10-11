e0fa4bf416b19f7e1fc84b91cc5000dab9e8476b
yashrewa <yashrewa00@gmail.com>
2023-10-11 21:28:30 +0530

    v8 migrated the whole application into next 13.5 with latest app router

--- src/app/utils/baseUrl.js
+++ src/app/utils/baseUrl.js
@@ -4,4 +4,4 @@
 
 // if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
 // export const baseUrl =  node_prod_env ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://127.0.0.1:3000'
-export const baseUrl =  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+export const baseUrl =  process.env.NODE_ENV==='development'? `${process.env.NEXT_PUBLIC_SITE_URL}`: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
