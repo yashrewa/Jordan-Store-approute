63ad3d279c9d118c9eb4b492bc79ff2212274d0b
yashrewa <yashrewa00@gmail.com>
2023-10-11 20:10:07 +0530

    v2 migrated the whole application into next 13.5 with latest app router

--- src/app/utils/baseUrl.js
+++ src/app/utils/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_ENV==='production'?procces.env.NEXT_PUBLIC_VERCEL_URL:'http://localhost:3000';
+export const baseUrl = process.env.VERCEL_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
