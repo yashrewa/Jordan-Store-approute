de93b38243eceacc1f3d6c05f5a5738e790384b5
yashrewa <yashrewa00@gmail.com>
2023-10-11 20:44:46 +0530

    v5 migrated the whole application into next 13.5 with latest app router

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL,
-    }
+    // env: {
+    //     NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL,
+    // }
 }
 
 module.exports = nextConfig
--- src/app/utils/baseUrl.js
+++ src/app/utils/baseUrl.js
@@ -1,13 +1,6 @@
-// const vercel_public_domain = process.env.NEXT_PUBLIC_DOMAIN
+const vercel_public_domain = process.env.NEXT_PUBLIC_DOMAIN
 
-// const node_prod_env = process.env.NODE_ENV === 'production'
-
-export const baseUrl =  process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000'
-
-
-// export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-
-
-
+const node_prod_env = process.env.NODE_ENV === 'production'
 
 // if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
+export const baseUrl =  vercel_public_domain ? vercel_public_domain : (node_prod_env ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://127.0.0.1:3000')
