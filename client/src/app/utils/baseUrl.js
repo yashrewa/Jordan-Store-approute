f85578314f445db586fa246a5b9ce2784c629e87
yashrewa <yashrewa00@gmail.com>
2024-02-23 14:22:46 +0530

    backend ported from somewhere to somewhere else due to cost issues

--- client/src/app/utils/baseUrl.js
+++ client/src/app/utils/baseUrl.js
@@ -5,4 +5,4 @@
 // if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
 // export const baseUrl =  node_prod_env ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://127.0.0.1:3000'
 // export const baseUrl = process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : `http://localhost:3000}`
-export const baseUrl = `https://strong-return-401509.el.r.appspot.com`
+export const baseUrl = `https://jordan-store-approute.onrender.com`
