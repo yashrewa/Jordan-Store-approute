79384c08745f679c6e449ce8f6c7af6149cf96d2
Yash Mishra <104890058+yashrewa@users.noreply.github.com>
2024-03-24 15:27:14 +0530

    Delete client/.next directory

--- client/.next/static/chunks/app/success/page-c587b655a283f97a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[60,290,91],{1571:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[724,971,864,744],function(){return n(n.s=1571)}),_N_E=n.O()}]);
