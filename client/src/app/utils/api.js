41403837a4327b8efa6d09d39b9637810e75ca60
yashalternate <164730925+yashalternate@users.noreply.github.com>
2024-03-24 15:31:39 +0530

    fixed server limit reached issue

--- client/src/app/utils/api.js
+++ client/src/app/utils/api.js
@@ -1,6 +1,7 @@
 
 export const UseFetchFromNext = async (endpoint) =>{
-    const res = await fetch(`https://jordan-store-approute.onrender.com${endpoint}`)
+    const res = await fetch(`https://jordan-store-backup-backend.onrender.com${endpoint}`)
+    // const res = await fetch(`https://jordan-store-approute.onrender.com${endpoint}`)
     const data = await res.json()
     console.log(data)
     return data;
